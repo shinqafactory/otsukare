6e9bd41c1c2ef4fedb793d083d842a561f87f4ec
築城賢司 <tsuiki@shinqafactory.com>
2013-08-04 15:09:27 +0900

    メッセージコントローラまで一応実装したよー デバイスのログインを実装する前に全ファイルpush

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,11 +13,54 @@ class MessagesController < ApplicationController
   
   # メッセージ詳細を表示する
   def detail
-    @message_new = Message.new
     @messages = Message.find(
       :all,
       :conditions => ["user_id = ? and (msg_from = ? or msg_to = ?)", params[:id], params[:msg_from], params[:msg_to]],
       :order => "created_at DESC"
     )
+    
+    #返信していなければ、返信用の値を設定
+    if @messages.count == 1
+      linkId = 0
+      #IDを取得
+      @messages.each do |messages|
+        linkId = messages.id
+      end
+      @message_new = Message.new(user_id: params[:id], msg_from: params[:id], msg_to: params[:msg_from], link_id: linkId)
+    else
+      #返信している場合は設定しない
+      @message_new = Message.new()
+    end
+    
   end
+
+  #messege送信
+  def send_messege
+    
+    # 初期値設定 TODO 本来はログインユーザー
+    @message_new = Message.new(link_id: params[:entry_id], msg_to: params[:msg_to], msg_from: 1, user_id: 1)
+    
+    #　enttry情報を取得する
+    @entry = Entry.find(
+      :all, 
+      :conditions => ["display_flg = ? and id = ?", 0, params[:entry_id]])
+  end
+    
+  #messege返信
+  def create
+    @message_new = Message.new(params[:message])
+    
+    respond_to do |format|
+      if @message_new.save
+        format.html { redirect_to :controller => 'messages', :action => 'show', :id => @message_new.user_id }
+      else
+        @entries = Entry.find(:all, :conditions => {:display_flg => 0}, :order => "created_at DESC")
+        format.html { render :template => "tops/index" }
+        format.json { render json: @message_new.errors, status: :unprocessable_entity }
+        format.json { render json: @entries }
+      end
+    end
+    @entry_new = Entry.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Otsukare::Application.routes.draw do
   root :to => 'tops#index'
   resources :tops, :only => [:index, :create, :destroy]
   resources :messages, :only => [:show, :create, :destroy]
-  #match ':top/' => 'tops#show'
 
+  #messege投稿
+  match 'messages/send_messege/:entry_id/:msg_to' => 'messages#send_messege', :via => :get
+  #messege詳細
   match 'messages/detail/:id/:msg_from/:msg_to' => 'messages#detail', :via => :get
   
   #ログインページ
